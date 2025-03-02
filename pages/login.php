219743b7d6a308aca17e9b8cb7e01ab5631b9414
CameliaAz <cameliaazzahra94@gmail.com>
2025-03-02 13:11:10 +0700

    fix login regist

--- pages/login.php
+++ pages/login.php
@@ -1,4 +1,5 @@
-<?php include '../includes/db.php'; 
+<?php 
+include '../includes/db.php'; 
 ?>
 
 <!DOCTYPE html>
@@ -13,11 +14,11 @@
         <input type="hidden" name="action" value="login">
         <div class="mb-4">
             <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email:</label>
-            <input type="email" name="email" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+            <input type="email" name="email" id="helper-text" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com">
         </div>
         <div class="mb-4">
             <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password:</label>
-            <input type="password" name="password" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+            <input type="password" name="password" id="helper-text" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </div>
         <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Login</button>
         <p class="text-center text-sm text-gray-700 mt-2">Belum memiliki akun? <a href="register.php" class="text-center text-blue-500 hover:text-blue-700">Daftar sekarang</a></p>
--- pages/register.php
+++ pages/register.php
@@ -1,4 +1,6 @@
-<?php include '../includes/db.php'; ?>
+<?php 
+include '../includes/db.php'; 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,16 +14,16 @@
     <input type="hidden" name="action" value="register">
     <div class="mb-4">
         <label for="nama_lengkap" class="block text-gray-700 text-sm font-bold mb-2">Nama Lengkap:</label>
-        <input type="text" name="nama_lengkap" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+        <input type="text" name="nama_lengkap" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Masukkan nama lengkap">
     </div>
     <div class="mb-4">
         <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email:</label>
-        <input type="email" name="email" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+        <input type="email" name="email" id="helper-text" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com">
     </div>
     <div class="mb-4">
     <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password:</label>
     <div class="flex items-center">
-        <input type="password" name="password" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password">
+    <input type="password" name="password" id="helper-text" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <button type="button" class="ml-2 text-gray-700" onclick="togglePassword('password')">
             <i id="password-icon" class="fas fa-eye-slash"></i>
         </button>
@@ -43,6 +45,7 @@
     }
 </script>
     <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Daftar</button>
+    <p class="text-center text-sm text-gray-700 mt-2">Sudah memiliki akun? <a href="login.php" class="text-center text-blue-500 hover:text-blue-700">Login sekarang</a></p>
 </form>
 
     <script src="https://cdn.tailwindcss.com"></script>
